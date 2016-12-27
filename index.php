ec3d73a90f4fcc6ec030a103b321527538de21b6
Devin Prince <semperverus@gmail.com>
2016-12-27 09:41:10 -0800

    second commit

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<title>Email Web Administration</title>
+<head></head>
+<body>
+  <div id="header">
+    Email Web Administration
+  </div>
+  <div id="management">
+    <?php  ?>
+  </div>
+  <div id="footer">
+    Email Web Administration, &copy;<?php
+  </div>
+</body>
+</html>
